9bd145245e2ecd9639371fca887fd1544a26be4f
Wagner Paulo <wagstalos@hotmail.com>
2026-01-19 15:56:20 -0300

    feat: added swiper

---
+++ js/scripts/slide-service.js
@@ -0,0 +1,19 @@
+function slideService() {
+  const swiper = new Swiper(".s-service__swiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    grabCursor: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+    },
+  });
+}
+
+slideService();
