717584c989ec024b7f4494c230221574c2f3e586
Wagner Paulo <wagstalos@hotmail.com>
2026-01-27 16:16:56 -0300

    feat: added page online

--- js/scripts/menuMobile.js
+++ js/scripts/menuMobile.js
@@ -1,25 +1,31 @@
-function menuMobile() {
+function initMenuMobile() {
   var dotsButton = document.querySelector(".dots");
   var menuMobile = document.querySelector(".menu-mobile");
   var closeIcon = document.querySelector(".icon-tabler-x");
   var links = document.querySelectorAll(".menu-mobile__links a");
 
-  dotsButton.addEventListener("click", function () {
-    menuMobile.style.display = "block";
-    closeIcon.style.display = "block";
-  });
+  if (dotsButton) {
+    dotsButton.addEventListener("click", function () {
+      menuMobile.style.display = "block";
+      closeIcon.style.display = "block";
+    });
+  }
 
-  closeIcon.addEventListener("click", function () {
-    menuMobile.style.display = "none";
-    closeIcon.style.display = "none";
-  });
-
-  links.forEach(function (link) {
-    link.addEventListener("click", function () {
+  if (closeIcon) {
+    closeIcon.addEventListener("click", function () {
       menuMobile.style.display = "none";
       closeIcon.style.display = "none";
     });
-  });
+  }
+
+  if (links) {
+    links.forEach(function (link) {
+      link.addEventListener("click", function () {
+        menuMobile.style.display = "none";
+        closeIcon.style.display = "none";
+      });
+    });
+  }
 }
 
-menuMobile();
+window.initMenuMobile = initMenuMobile;
--- js/scripts/sticky-header.js
+++ js/scripts/sticky-header.js
@@ -1,14 +1,16 @@
-function stickyHeader() {
+function initStickyHeader() {
   const header = document.querySelector("header");
   const logo = document.querySelector(".logo"); // Select the logo element
 
   window.addEventListener("scroll", function () {
-    if (window.scrollY > 0) {
-      header.classList.add("scrolled");
-    } else {
-      header.classList.remove("scrolled");
+    if (header) {
+      if (window.scrollY > 0) {
+        header.classList.add("scrolled");
+      } else {
+        header.classList.remove("scrolled");
+      }
     }
   });
 }
 
-stickyHeader();
+window.initStickyHeader = initStickyHeader;
