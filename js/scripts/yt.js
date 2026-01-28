c050aac34a6ec2a2bd06bdf573eea1ae7ecd0daf
Wagner Paulo <wagstalos@hotmail.com>
2026-01-28 10:52:27 -0300

    feat: Introduce YouTube video player functionality, new button styles, and integrate AOS animations across pages.

--- js/scripts/yt.js
+++ js/scripts/yt.js
@@ -1,7 +1,16 @@
-document
-  .getElementById("video-thumbnail")
-  .addEventListener("click", function () {
-    var youtubeDiv = document.getElementById("youtube-video");
-    youtubeDiv.innerHTML =
-      '<iframe width="70%" height="515" src="https://www.youtube.com/embed/0t3_GjHhOTw?si=crY-y94fTVrP2XjL&autoplay=1&controls=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
-  });
+const videoThumbnail = document.getElementById("video-thumbnail");
+const playButton = document.querySelector(".play-button");
+
+const loadVideo = function () {
+  var youtubeDiv = document.getElementById("youtube-video");
+  youtubeDiv.innerHTML =
+    '<iframe width="70%" height="515" src="https://www.youtube.com/embed/0t3_GjHhOTw?si=crY-y94fTVrP2XjL&autoplay=1&controls=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+};
+
+if (videoThumbnail) {
+  videoThumbnail.addEventListener("click", loadVideo);
+}
+
+if (playButton) {
+  playButton.addEventListener("click", loadVideo);
+}
